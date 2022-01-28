6dc3ce8052e4a01eb2dd82f0d80044f165603813
Alexey Chuvagin <alexeychuvagin@me.com>
2022-01-28 11:01:55 +0300

    Update Program.cs

--- src/BlazorWordle/Program.cs
+++ src/BlazorWordle/Program.cs
@@ -7,7 +7,11 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddHttpClient<WordsClient>(cfg => cfg.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + "/"));
+
+builder.Services.AddHttpClient<WordsClient>(cfg =>
+    cfg.BaseAddress = builder.HostEnvironment.IsProduction()
+        ? new Uri(builder.HostEnvironment.BaseAddress + "/BlazorWordle/")
+        : new Uri(builder.HostEnvironment.BaseAddress));
 builder.Services.AddTransient<GameState>();
 
 await builder.Build().RunAsync();
